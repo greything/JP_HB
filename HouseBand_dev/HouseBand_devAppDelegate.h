fd344f580382e706bb22038fc48591ad67d7f1e2
Greything <karl@greything.net>
2011-10-26 15:08:36 -0400

    we added a circle and the images folder to assests

--- HouseBand_dev/HouseBand_devAppDelegate.h
+++ HouseBand_dev/HouseBand_devAppDelegate.h
@@ -5,6 +5,7 @@
 //  Created by Marilys Ernst on 10/3/11.
 //  Copyright 2011 MI Artbox, Incorporated. All rights reserved.
 //  my contribution - karl!
+// a change to the project
 
 #import <UIKit/UIKit.h>
 
