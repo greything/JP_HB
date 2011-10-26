000a6fe28ae4c4c570743ce6971de8c27588fd7e
Greything <karl@greything.net>
2011-10-26 16:00:07 -0400

    a change for the purpose of demonstrating git

--- HouseBand_dev/HouseBand_devViewController.h
+++ HouseBand_dev/HouseBand_devViewController.h
@@ -4,7 +4,7 @@
 //
 //  Created by Marilys Ernst on 10/3/11.
 //  Copyright 2011 MI Artbox, Incorporated. All rights reserved.
-//
+// a change
 
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
